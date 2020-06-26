fd7d716934fa6a17b4786485d37d3654011661b8
dfowl <d.ribchinski@gmail.com>
2020-06-26 14:18:47 -0400

    Added Serial Version ID and created KmToMiPanel class

--- MetricToImperial/src/converter/ConverterFrame.java
+++ MetricToImperial/src/converter/ConverterFrame.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 
 public class ConverterFrame extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
 	public ConverterFrame() {
 		init();
 	}
---
+++ MetricToImperial/src/converter/KmToMiPanel.java
@@ -0,0 +1,16 @@
+package converter;
+
+import javax.swing.JPanel;
+
+public class KmToMiPanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+
+	public KmToMiPanel() {
+		init();
+	}
+	
+	private void init() {
+		
+	}
+}
