b170a6729c555fabc6c63a57bc0758662bfdbf90
dfowl <d.ribchinski@gmail.com>
2020-06-26 14:35:56 -0400

    Made basic layout of KmToMiPanel

--- MetricToImperial/src/converter/KmToMiPanel.java
+++ MetricToImperial/src/converter/KmToMiPanel.java
@@ -1,16 +1,40 @@
 package converter;
 
-import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class KmToMiPanel extends JPanel {
-	
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class KmToMiPanel extends JPanel implements ActionListener {
+
 	private static final long serialVersionUID = 1L;
+	
+	private JTextField input;
+	private JTextField output;
+	private JButton convertButton;
 
 	public KmToMiPanel() {
+		input = new JTextField(20);
+		output = new JTextField(20);
+		convertButton = new JButton("Convert");
+		
 		init();
 	}
-	
+
 	private void init() {
+		setLayout(new GridLayout(1, 3));
+		add(input);
+		add(output);
+		add(convertButton);
+		
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
 	}
 }
