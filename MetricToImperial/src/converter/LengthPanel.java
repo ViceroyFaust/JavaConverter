0923b39cf0669f81055c8b5cf261b5615771dad0
dfowl <d.ribchinski@gmail.com>
2020-07-02 20:54:57 -0400

    Basic structure of ActionListener for LengthPanel

--- MetricToImperial/src/converter/LengthPanel.java
+++ MetricToImperial/src/converter/LengthPanel.java
@@ -3,8 +3,6 @@ package converter;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
