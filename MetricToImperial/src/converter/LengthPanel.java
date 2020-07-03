f2560cc574816d60352e3e3d3d3738c8a8dd012f
dfowl <d.ribchinski@gmail.com>
2020-07-02 20:53:40 -0400

    Set up basic structure of actionPerformed

--- MetricToImperial/src/converter/LengthPanel.java
+++ MetricToImperial/src/converter/LengthPanel.java
@@ -3,7 +3,8 @@ package converter;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -22,15 +23,15 @@ public class LengthPanel extends JPanel implements ActionListener {
 	private JButton convertButton;
 
 	public LengthPanel() {
-		String[] metric = {"Kilometre", "Metre", "Decimetre", "Centimetre", "Millimetre"};
-		String[] imperial = {"Mile", "Yard", "Foot", "Inch"};
+		String[] metric = { "Kilometre", "Metre", "Decimetre", "Centimetre", "Millimetre" };
+		String[] imperial = { "Mile", "Yard", "Foot", "Inch" };
 
 		mBox = new JComboBox<String>(metric);
 		imBox = new JComboBox<String>(imperial);
 		input = new JTextField(12);
 		output = new JTextField(12);
 		convertButton = new JButton("Convert");
-		
+
 		init();
 	}
 
@@ -38,22 +39,65 @@ public class LengthPanel extends JPanel implements ActionListener {
 		setLayout(new GridLayout(3, 2, 15, 7));
 		setSize(468, 110);
 		setBorder(BorderFactory.createEmptyBorder(15, 10, 15, 10));
-		
+
 		add(mBox);
 		add(input);
-		
+
 		add(imBox);
 		add(output);
+
 		add(convertButton);
-		
+
+		mBox.addActionListener(this);
+		imBox.addActionListener(this);
+
 		convertButton.addActionListener(this);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		DecimalFormat format = new DecimalFormat("#,##0.###");
-		double km = Double.parseDouble(input.getText());
-		double mi = km / 1.609344;
-		output.setText(format.format(mi));
+		if (e.getSource() == mBox) {
+			switch (mBox.getSelectedIndex()) {
+			case 0: // Kilometre
+				System.out.println("Kilometre");
+				break;
+			case 1: // Metre
+				System.out.println("Metre");
+				break;
+
+			case 2: // Decimetre
+				System.out.println("Decimetre");
+				break;
+			case 3: // Centimetre
+				System.out.println("Centimetre");
+				break;
+			case 4: // Millimetre
+				System.out.println("Millimetre");
+				break;
+			default:
+				System.out.println("This should not be possible");
+				break;
+			}
+		}
+		if (e.getSource() == imBox) {
+			switch (imBox.getSelectedIndex()) {
+			case 0: // Mile
+				System.out.println("Mile");
+				break;
+			case 1: // Yard
+				System.out.println("Yard");
+				break;
+			case 2: // Foot
+				System.out.println("Foot");
+				break;
+			case 3: // Inch
+				System.out.println("Inch");
+				break;
+			default:
+				System.out.println("This should not be possible");
+				break;
+			}
+		}
 	}
 }
