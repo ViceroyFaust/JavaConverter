b835971fe2485b2d6d7d41b39019d0cc8701d3b4
dfowl <d.ribchinski@gmail.com>
2020-07-04 07:21:15 -0400

    Defined conversion factors between metres and imperial units

--- MetricToImperial/src/converter/Converter.java
+++ MetricToImperial/src/converter/Converter.java
@@ -1,6 +1,11 @@
 package converter;
 
 public class Converter {
+	
+	public static final double METRES_TO_MILES = 6.213712E-4;
+	public static final double METRES_TO_YARDS = 1.093613;
+	public static final double METRES_TO_FEET = 3.28084;
+	public static final double METRES_TO_INCHES = 39.37;
 
 	public static void main(String[] args) {
 		new ConverterFrame();
