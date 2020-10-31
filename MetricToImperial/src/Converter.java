bdee2358c379b4b95aabb2448fd5b06c4bb7f9dd
DFOwl <d.ribchinski@gmail.com>
2020-10-31 14:29:11 -0400

    Removed "0 output" when input is less than zero

--- MetricToImperial/src/Converter.java
+++ MetricToImperial/src/Converter.java
@@ -57,214 +57,213 @@ public final class Converter {
      *  Temperature = -T */
 
     public static double convert(String fromUnit, String toUnit, double measure) {
-        double output = -1;
-        if (measure >= 0) {
-            switch (fromUnit) { // At first the unit is converted into SI one place
-                /* MEGA UNITS */
-                case "TONNES-M":
-                    output = measure * MEGA;
-                    break;
-                /* KILO UNITS */
-                case "KILOMETRES-L":
-                case "KILOGRAMMES-M":
-                case "CUBIC METRES-V":
-                    output = measure * KILO;
-                    break;
-                /* ONES UNITS */
-                case "METRES-L":
-                case "GRAMMES-M":
-                case "LITRES-V":
-                case "KELVIN-T":
-                    output = measure;
-                    break;
-                /* DECI UNITS */
-                case "DECIMETRES-L":
-                    output = measure * DECI;
-                    break;
-                /* CENTI UNITS */
-                case "CENTIMETRES-L":
-                    output = measure * CENTI;
-                    break;
-                /* MILLI UNITS */
-                case "MILLIMETRES-L":
-                case "MILLIGRAMMES-M":
-                case "MILLILITRES-V":
-                    output = measure * MILLI;
-                    break;
-                /* LENGTHS */
-                case "MILES-L":
-                    output = measure * MILES_TO_METRES;
-                    break;
-                case "YARDS-L":
-                    output = measure * YARDS_TO_METRES;
-                    break;
-                case "FEET-L":
-                    output = measure * FEET_TO_METRES;
-                    break;
-                case "INCHES-L":
-                    output = measure * INCHES_TO_METRES;
-                    break;
-                /* MASS */
-                case "TONS-M":
-                    output = measure * TONS_TO_G;
-                    break;
-                case "POUNDS-M":
-                    output = measure * POUNDS_TO_G;
-                    break;
-                case "OUNCES-M":
-                    output = measure * OUNCES_TO_G;
-                    break;
-                /* VOLUMES */
-                case "CUPS-V":
-                    output = measure * MCUP_TO_L;
-                    break;
-                case "TABLESPOON-V":
-                    output = measure * MTBSP_TO_L;
-                    break;
-                case "TEASPOON-V":
-                    output = measure * MTSP_TO_L;
-                    break;
-                case "CUBIC FEET-V":
-                    output = measure * CFT_TO_L;
-                    break;
-                case "GALLONS-V":
-                    output = measure * GALLONS_TO_L;
-                    break;
-                case "QUARTS-V":
-                    output = measure * QUARTS_TO_L;
-                    break;
-                case "PINTS-V":
-                    output = measure * PINTS_TO_L;
-                    break;
-                case "US CUPS-V":
-                    output = measure * USCUP_TO_L;
-                    break;
-                case "FLUID OUNCES-V":
-                    output = measure * FLOUNCE_TO_L;
-                    break;
-                case "CUBIC INCHES-V":
-                    output = measure * CIN_TO_L;
-                    break;
-                case "US TABLESPOON-V":
-                    output = measure * USTBSP_TO_L;
-                    break;
-                case "US TEASPOON-V":
-                    output = measure * USTSP_TO_L;
-                    break;
-                /* TEMPERATURES */
-                case "CELSIUS-T":
-                    output = measure + 273.15;
-                    break;
-                case "FAHRENHEIT-T":
-                    output = (measure + 459.67) * (5.0 / 9.0);
-                    break;
-                default:
-                    output = -1; // For unrecognised input
-            }
-
-            switch (toUnit) { // At first the unit is converted into SI one place
-                /* MEGA UNITS */
-                case "TONNES-M":
-                    output /= MEGA;
-                    break;
-                /* KILO UNITS */
-                case "KILOMETRES-L":
-                case "KILOGRAMMES-M":
-                case "CUBIC METRES-V":
-                    output /= KILO;
-                    break;
-                /* ONES UNITS */
-                case "METRES-L":
-                case "GRAMMES-M":
-                case "LITRES-V":
-                case "KELVIN-T":
-                    output *= 1;
-                    break;
-                /* DECI UNITS */
-                case "DECIMETRES-L":
-                    output /= DECI;
-                    break;
-                /* CENTI UNITS */
-                case "CENTIMETRES-L":
-                    output /= CENTI;
-                    break;
-                /* MILLI UNITS */
-                case "MILLIMETRES-L":
-                case "MILLIGRAMMES-M":
-                case "MILLILITRES-V":
-                    output /= MILLI;
-                    break;
-                /* LENGTHS */
-                case "MILES-L":
-                    output /= MILES_TO_METRES;
-                    break;
-                case "YARDS-L":
-                    output /= YARDS_TO_METRES;
-                    break;
-                case "FEET-L":
-                    output /= FEET_TO_METRES;
-                    break;
-                case "INCHES-L":
-                    output /= INCHES_TO_METRES;
-                    break;
-                /* MASS */
-                case "TONS-M":
-                    output /= TONS_TO_G;
-                    break;
-                case "POUNDS-M":
-                    output /= POUNDS_TO_G;
-                    break;
-                case "OUNCES-M":
-                    output /= OUNCES_TO_G;
-                    break;
-                /* VOLUMES */
-                case "CUPS-V":
-                    output /= MCUP_TO_L;
-                    break;
-                case "TABLESPOON-V":
-                    output /= MTBSP_TO_L;
-                    break;
-                case "TEASPOON-V":
-                    output /= MTSP_TO_L;
-                    break;
-                case "CUBIC FEET-V":
-                    output /= CFT_TO_L;
-                    break;
-                case "GALLONS-V":
-                    output /= GALLONS_TO_L;
-                    break;
-                case "QUARTS-V":
-                    output /= QUARTS_TO_L;
-                    break;
-                case "PINTS-V":
-                    output /= PINTS_TO_L;
-                    break;
-                case "US CUPS-V":
-                    output /= USCUP_TO_L;
-                    break;
-                case "FLUID OUNCES-V":
-                    output /= FLOUNCE_TO_L;
-                    break;
-                case "CUBIC INCHES-V":
-                    output /= CIN_TO_L;
-                    break;
-                case "US TABLESPOON-V":
-                    output /= USTBSP_TO_L;
-                    break;
-                case "US TEASPOON-V":
-                    output /= USTSP_TO_L;
-                    break;
-                /* TEMPERATURES */
-                case "CELSIUS-T":
-                    output -= 273.15;
-                    break;
-                case "FAHRENHEIT-T":
-                    output = output * (9.0 / 5.0) - 459.67;
-                    break;
-                default:
-                    output = -1; // For unrecognised input
-            }
+        double output;
+        switch (fromUnit) { // At first the unit is converted into SI ones place
+            /* MEGA UNITS */
+            case "TONNES-M":
+                output = measure * MEGA;
+                break;
+            /* KILO UNITS */
+            case "KILOMETRES-L":
+            case "KILOGRAMMES-M":
+            case "CUBIC METRES-V":
+                output = measure * KILO;
+                break;
+            /* ONES UNITS */
+            case "METRES-L":
+            case "GRAMMES-M":
+            case "LITRES-V":
+            case "KELVIN-T":
+                output = measure;
+                break;
+            /* DECI UNITS */
+            case "DECIMETRES-L":
+                output = measure * DECI;
+                break;
+            /* CENTI UNITS */
+            case "CENTIMETRES-L":
+                output = measure * CENTI;
+                break;
+            /* MILLI UNITS */
+            case "MILLIMETRES-L":
+            case "MILLIGRAMMES-M":
+            case "MILLILITRES-V":
+                output = measure * MILLI;
+                break;
+            /* LENGTHS */
+            case "MILES-L":
+                output = measure * MILES_TO_METRES;
+                break;
+            case "YARDS-L":
+                output = measure * YARDS_TO_METRES;
+                break;
+            case "FEET-L":
+                output = measure * FEET_TO_METRES;
+                break;
+            case "INCHES-L":
+                output = measure * INCHES_TO_METRES;
+                break;
+            /* MASS */
+            case "TONS-M":
+                output = measure * TONS_TO_G;
+                break;
+            case "POUNDS-M":
+                output = measure * POUNDS_TO_G;
+                break;
+            case "OUNCES-M":
+                output = measure * OUNCES_TO_G;
+                break;
+            /* VOLUMES */
+            case "CUPS-V":
+                output = measure * MCUP_TO_L;
+                break;
+            case "TABLESPOON-V":
+                output = measure * MTBSP_TO_L;
+                break;
+            case "TEASPOON-V":
+                output = measure * MTSP_TO_L;
+                break;
+            case "CUBIC FEET-V":
+                output = measure * CFT_TO_L;
+                break;
+            case "GALLONS-V":
+                output = measure * GALLONS_TO_L;
+                break;
+            case "QUARTS-V":
+                output = measure * QUARTS_TO_L;
+                break;
+            case "PINTS-V":
+                output = measure * PINTS_TO_L;
+                break;
+            case "US CUPS-V":
+                output = measure * USCUP_TO_L;
+                break;
+            case "FLUID OUNCES-V":
+                output = measure * FLOUNCE_TO_L;
+                break;
+            case "CUBIC INCHES-V":
+                output = measure * CIN_TO_L;
+                break;
+            case "US TABLESPOON-V":
+                output = measure * USTBSP_TO_L;
+                break;
+            case "US TEASPOON-V":
+                output = measure * USTSP_TO_L;
+                break;
+            /* TEMPERATURES */
+            case "CELSIUS-T":
+                output = measure + 273.15;
+                break;
+            case "FAHRENHEIT-T":
+                output = (measure + 459.67) * (5.0 / 9.0);
+                break;
+            default:
+                output = -1; // For unrecognised input
         }
+
+        switch (toUnit) { // At first the unit is converted into SI one place
+            /* MEGA UNITS */
+            case "TONNES-M":
+                output /= MEGA;
+                break;
+            /* KILO UNITS */
+            case "KILOMETRES-L":
+            case "KILOGRAMMES-M":
+            case "CUBIC METRES-V":
+                output /= KILO;
+                break;
+            /* ONES UNITS */
+            case "METRES-L":
+            case "GRAMMES-M":
+            case "LITRES-V":
+            case "KELVIN-T":
+                output *= 1;
+                break;
+            /* DECI UNITS */
+            case "DECIMETRES-L":
+                output /= DECI;
+                break;
+            /* CENTI UNITS */
+            case "CENTIMETRES-L":
+                output /= CENTI;
+                break;
+            /* MILLI UNITS */
+            case "MILLIMETRES-L":
+            case "MILLIGRAMMES-M":
+            case "MILLILITRES-V":
+                output /= MILLI;
+                break;
+            /* LENGTHS */
+            case "MILES-L":
+                output /= MILES_TO_METRES;
+                break;
+            case "YARDS-L":
+                output /= YARDS_TO_METRES;
+                break;
+            case "FEET-L":
+                output /= FEET_TO_METRES;
+                break;
+            case "INCHES-L":
+                output /= INCHES_TO_METRES;
+                break;
+            /* MASS */
+            case "TONS-M":
+                output /= TONS_TO_G;
+                break;
+            case "POUNDS-M":
+                output /= POUNDS_TO_G;
+                break;
+            case "OUNCES-M":
+                output /= OUNCES_TO_G;
+                break;
+            /* VOLUMES */
+            case "CUPS-V":
+                output /= MCUP_TO_L;
+                break;
+            case "TABLESPOON-V":
+                output /= MTBSP_TO_L;
+                break;
+            case "TEASPOON-V":
+                output /= MTSP_TO_L;
+                break;
+            case "CUBIC FEET-V":
+                output /= CFT_TO_L;
+                break;
+            case "GALLONS-V":
+                output /= GALLONS_TO_L;
+                break;
+            case "QUARTS-V":
+                output /= QUARTS_TO_L;
+                break;
+            case "PINTS-V":
+                output /= PINTS_TO_L;
+                break;
+            case "US CUPS-V":
+                output /= USCUP_TO_L;
+                break;
+            case "FLUID OUNCES-V":
+                output /= FLOUNCE_TO_L;
+                break;
+            case "CUBIC INCHES-V":
+                output /= CIN_TO_L;
+                break;
+            case "US TABLESPOON-V":
+                output /= USTBSP_TO_L;
+                break;
+            case "US TEASPOON-V":
+                output /= USTSP_TO_L;
+                break;
+            /* TEMPERATURES */
+            case "CELSIUS-T":
+                output -= 273.15;
+                break;
+            case "FAHRENHEIT-T":
+                output = output * (9.0 / 5.0) - 459.67;
+                break;
+            default:
+                output = -1; // For unrecognised input
+        }
+
         return output;
     }
 }
